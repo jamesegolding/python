bug fixes and quaternion integration

drone/control.py
@@ -1,6 +1,8 @@
 import scipy.linalg
 import numpy as np
 import numba
+
+import drone
 import utilities as utils
 import quaternion
 from parameters import *
@@ -11,11 +13,11 @@ logger.setLevel(logging.INFO)
 
 
 # attitude control parameters
-K_P = 0.2
-K_V = 0.5
+K_P = 0.1
+K_V = 0.3
 K_Q = 2.0
 K_W = 0.9
-THETA_MAX = np.pi / 4.
+THETA_MAX = np.pi / 8.
 
 
 def lqr_gain(A, B, Q, R):
@@ -28,21 +30,23 @@ def lqr_gain(A, B, Q, R):
 
     return k
 
+# control derived parameters
+MOTOR_INV = drone.torque_motor_inv()
+A, B, Q, R, U_0 = drone.vertical_state_space()
+K_Z = lqr_gain(A, B, Q, R)
+
 
 def update(s: np.ndarray,
            tgt: np.ndarray,
-           k_z: np.ndarray,
-           motor_inv: np.ndarray,
-           u_0: np.ndarray,
            ):
 
-    u_attitude = attitude(s, tgt[np.array([0, 1])], motor_inv)
-    u_vertical = vertical(s, tgt[2], k_z)
+    u_attitude = attitude(s, tgt[np.array([0, 1])])
+    u_vertical = vertical(s, tgt[2])
 
     if max(u_attitude) - min(u_attitude) > f_motor_max:
         u = 0.5 * f_motor_max * np.ones(4) + u_attitude
     else:
-        u = u_0 + u_vertical + u_attitude
+        u = U_0 + u_vertical + u_attitude
         if max(u) > f_motor_max:
             u = u - (max(u) - f_motor_max)
 
@@ -54,12 +58,11 @@ def update(s: np.ndarray,
 @numba.jit(nopython=True)
 def vertical(s: np.ndarray,
              tgt_z: np.ndarray,
-             k: np.ndarray,
              ):
 
     e_z = np.array([s[2], s[9]]) - np.array([tgt_z, 0])
 
-    u = -k @ e_z
+    u = -K_Z @ e_z
 
     # scale by vertical component of orientation
     q_real = s[3]
@@ -70,27 +73,24 @@ def vertical(s: np.ndarray,
 
 def attitude(s: np.ndarray,
              tgt_xy_psi: np.ndarray,
-             motor_inv: np.ndarray,
              ):
 
     # get states
     q = s[3:7]   # quaternion (world to body)
-    o = s[10:]   # quaternion rate (world to body)
-
-    # rotational velocities
-    w_body = quaternion.rate_matrix_body(q)
-    e_w = quaternion.to_angular_velocity(w_body, o)
+    n_body = s[10:13]   # quaternion rate (world to body)
 
     # position targets
     e_p = tgt_xy_psi[np.array([0, 1])] - s[np.array([0, 1])]
     e_v = np.array([0, 0]) - s[np.array([7, 8])]
-    q_r = planar_ctrl_law(e_p, e_v)
+
+    q_r = np.array([1., 0., 0., 0.])
+    #q_r = planar_ctrl_law(e_p, e_v)
 
     # overall target quaternion
     e_q = quaternion.product(q_r, quaternion.conjugate(q))[1:]
 
-    m_xy = K_Q * e_q - K_W * e_w
-    u = motor_inv @ np.concatenate((np.array([0.]), m_xy))
+    m_xy = K_Q * e_q - K_W * n_body
+    u = MOTOR_INV @ np.concatenate((np.array([0.]), m_xy))
 
     return u
 