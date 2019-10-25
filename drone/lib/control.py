quaternion contiguous array fix

drone/lib/control.py
@@ -2,7 +2,9 @@ import scipy.linalg
 import numpy as np
 import numba
 
-from lib import drone, quaternion, utilities as utils
+from lib import drone
+from lib import quaternion
+from lib import utilities as utils
 from lib.parameters import *
 
 import logging
@@ -34,6 +36,7 @@ A, B, Q, R, U_0 = drone.vertical_state_space()
 K_Z = lqr_gain(A, B, Q, R)
 
 
+@numba.jit(nopython=True)
 def update(s: np.ndarray,
            tgt: np.ndarray,
            ):
@@ -82,7 +85,6 @@ def attitude(s: np.ndarray,
     e_p = tgt_xy_psi[np.array([0, 1])] - s[np.array([0, 1])]
     e_v = np.array([0, 0]) - s[np.array([7, 8])]
 
-    #q_r = np.array([1., 0., 0., 0.])
     q_r = planar_ctrl_law(e_p, e_v)
 
     # overall target quaternion

drone/lib/quaternion.py
@@ -435,6 +435,6 @@ def rot_mat_der(q: np.ndarray, i_q: int = 0, b_inverse: bool = False):
         return np.nan * np.ones((3, 3))
 
     if b_inverse:
-        return np.transpose(np.add(u_dv_dt, v_du_dt))
+        return np.ascontiguousarray(np.add(u_dv_dt, v_du_dt).T)
     else:
         return np.add(u_dv_dt, v_du_dt)

drone/lib/sensor.py
@@ -44,7 +44,7 @@ def from_state(s: np.ndarray, g: np.ndarray, r_scale: float = 1.):
     return sensor_state
 
 
-@numba.jit(nopython=True)
+#@numba.jit(nopython=True)
 def to_state(sensor_state: Sensor,
              filter_state: Filter,
              u: np.ndarray,
@@ -220,7 +220,6 @@ def kalman_orientation(sensor_state: Sensor,
     ]))
 
     s = h_mat @ p_pred @ h_mat.T + r
-
     k = p_pred @ h_mat.T @ np.linalg.inv(s)
 
     x_update = x_pred + k @ y

drone/run_sim.py
@@ -6,7 +6,6 @@ from lib import drone
 from lib import quaternion
 from lib import sensor
 from lib import utilities as utils
-from lib.parameters import *
 import post_process
 
 import logging
@@ -37,7 +36,7 @@ def run_sim(t: np.ndarray, target: np.ndarray,
     p = np.zeros((11, 11))
 
     # initialize filter
-    filter_state = sensor.Filter(s=s0, p=p, r_trans_sensor=0., r_madgwick_gain=0.02)
+    filter_state = sensor.Filter(s=s0, p=p, r_trans_sensor=0.3, r_madgwick_gain=0.02)
 
     print("Starting simulation...")
 